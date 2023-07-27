4c7941331e76a3cc0b519e03d331df4f8b8a48a0
Benoit Havet <benhavet@WKMZTD031224.global.publicisgroupe.net>
2023-07-27 16:36:14 +0200

    add tabs component style and stories

---
+++ src/components/Tabs/Tabs..tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./Tabs.css";
+
+interface Tab {
+  label: string;
+  content: React.ReactNode;
+}
+
+interface TabProps {
+  tabs: Tab[];
+  onAction?: () => void;
+  dark?: boolean;
+}
+
+export const TabComponent: React.FC<TabProps> = ({
+  tabs,
+  dark = false,
+  ...props
+}) => {
+  const [activeTab, setActiveTab] = useState<number>(0);
+
+  const handleTabClick = (index: number) => {
+    setActiveTab(index);
+  };
+
+  const darkClassName = dark ? "--dark-mode" : "";
+
+  return (
+    <div className={`tab-component${darkClassName}`}>
+      <div className="tab-header">
+        {tabs.map((tab, index) => (
+          <button
+            key={index}
+            className={
+              index === activeTab
+                ? `active${darkClassName}`
+                : `button${darkClassName}`
+            }
+            onClick={() => {
+              handleTabClick(index);
+              props?.onAction?.();
+            }}
+            {...props}
+          >
+            {tab.label}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Tabs/Tabs.stories.tsx
@@ -0,0 +1,48 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { TabComponent } from "./Tabs.";
+import { action } from "@storybook/addon-actions";
+
+/**
+ *
+ * Composant permetant de créer un système d'onlgets
+ *
+ *  ### Usage
+ *
+ * ```jsx
+ * const tabs = [
+ * { label: "Tab 1", content: <p>Tab 1</p> },
+ * { label: "Tab 2", content: <p>Tab 2</p> },
+ * { label: "Tab 3", content: <p>Tab 3</p> }];
+ *
+ * <TabComponent tabs={tabs}>
+ * ```
+ */
+
+const meta = {
+  title: "UI/Tabs",
+  component: TabComponent,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof TabComponent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const tabs = [
+  { label: "Home", content: <p>Home</p> },
+  { label: "Page 1", content: <p>Page 1</p> },
+  { label: "Page 2", content: <p>Page 2</p> },
+];
+
+export const Default = () => {
+  return <TabComponent tabs={tabs} onAction={action("Tab clicked")} />;
+};
+
+export const Dark: Story = {
+  args: {
+    dark: true,
+    tabs: tabs,
+  },
+};
